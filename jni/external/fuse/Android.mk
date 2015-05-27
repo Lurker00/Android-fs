7b307ab6eb1c97e89ae172e0b15a1319b4902ad0
Lurker00 <Lurker00@users.noreply.github.com>
2015-05-27 11:54:58 +0300

    fuse Android framework build with NDK r10d

--- jni/external/fuse/Android.mk
+++ jni/external/fuse/Android.mk
@@ -16,6 +16,8 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
+LOCAL_CFLAGS := -Wall -O2 -std=c99
+
 LOCAL_SRC_FILES := \
 	buffer.c \
 	cuse_lowlevel.c \
@@ -46,21 +48,3 @@ LOCAL_MODULE := libfuse
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_STATIC_LIBRARY)
-#include $(BUILD_SHARED_LIBRARY)
-
-include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES := \
-	fusexmp.c
-
-LOCAL_C_INCLUDES := \
-	$(LOCAL_PATH)/include
-
-LOCAL_CFLAGS := -D_FILE_OFFSET_BITS=64
-
-LOCAL_MODULE := fusexmp
-LOCAL_MODULE_TAGS := optional
-
-LOCAL_STATIC_LIBRARIES := libfuse
-
-include $(BUILD_EXECUTABLE)
