d995519dc99893c2452e2db24c1b62c0283736a5
Lurker00 <Lurker00@users.noreply.github.com>
2015-06-03 13:07:52 +0300

    ntfs-3g refined Android project

--- jni/ntfs-3g/Android.mk
+++ jni/ntfs-3g/Android.mk
@@ -1,3 +1,4 @@
+CC_OPTIONS := -std=c99 -O2 -W -Wall -D_LARGEFILE_SOURCE -D_FILE_OFFSET_BITS=64 -DHAVE_CONFIG_H
 
 # libfuse lite
 LOCAL_PATH := $(call my-dir)
@@ -8,10 +9,9 @@ LOCAL_SRC_FILES := \
 	libfuse-lite/fuse_opt.c libfuse-lite/fuse_session.c libfuse-lite/fuse_signals.c\
 	libfuse-lite/helper.c libfuse-lite/mount.c libfuse-lite/mount_util.c androidglue/statvfs.c
 LOCAL_C_INCLUDES :=$(LOCAL_PATH)/include/fuse-lite $(LOCAL_PATH)/androidglue/include
-LOCAL_CFLAGS := -O2 -g -W -Wall -D_LARGEFILE_SOURCE -D_FILE_OFFSET_BITS=64  -DHAVE_CONFIG_H
+LOCAL_CFLAGS := $(CC_OPTIONS)
 LOCAL_MODULE := libfuse-lite
 LOCAL_MODULE_TAGS := eng
-LOCAL_SYSTEM_SHARED_LIBRARIES := libc libcutils
 include $(BUILD_STATIC_LIBRARY)
 
 # libntfs-3g
@@ -25,10 +25,9 @@ LOCAL_SRC_FILES :=libntfs-3g/acls.c libntfs-3g/attrib.c libntfs-3g/attrlist.c \
 	libntfs-3g/runlist.c libntfs-3g/security.c libntfs-3g/unistr.c\
 	libntfs-3g/unix_io.c libntfs-3g/volume.c libntfs-3g/realpath.c
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/include/fuse-lite  $(LOCAL_PATH)/include/ntfs-3g
-LOCAL_CFLAGS := -O2 -g -W -Wall -D_LARGEFILE_SOURCE -D_FILE_OFFSET_BITS=64  -DHAVE_CONFIG_H
+LOCAL_CFLAGS := $(CC_OPTIONS)
 LOCAL_MODULE := libntfs-3g-lib
 LOCAL_MODULE_TAGS := eng
-LOCAL_SYSTEM_SHARED_LIBRARIES := libc libcutils
 include $(BUILD_STATIC_LIBRARY)
 
 #ntfs-3g
@@ -37,10 +36,9 @@ LOCAL_SRC_FILES := src/ntfs-3g.c src/ntfs-3g_common.c
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/include/fuse-lite $(LOCAL_PATH)/include/ntfs-3g \
 			$(LOCAL_PATH)/androidglue/include $(LOCAL_PATH)/src
-LOCAL_CFLAGS := -O2 -g -W -Wall -D_LARGEFILE_SOURCE -D_FILE_OFFSET_BITS=64 -DHAVE_CONFIG_H
+LOCAL_CFLAGS := $(CC_OPTIONS)
 LOCAL_MODULE := ntfs-3g
 LOCAL_MODULE_TAGS := eng
-LOCAL_SYSTEM_SHARED_LIBRARIES := libc
 LOCAL_STATIC_LIBRARIES := libfuse-lite libntfs-3g-lib
 include $(BUILD_EXECUTABLE)
 
@@ -50,11 +48,9 @@ LOCAL_SRC_FILES := ntfsprogs/ntfsfix.c ntfsprogs/utils.c
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/include/fuse-lite $(LOCAL_PATH)/include/ntfs-3g \
 			$(LOCAL_PATH)/androidglue/include $(LOCAL_PATH)/ntfsprogs/
-LOCAL_CFLAGS := -O2 -g -W -Wall -D_LARGEFILE_SOURCE -D_FILE_OFFSET_BITS=64 -DHAVE_CONFIG_H
+LOCAL_CFLAGS := $(CC_OPTIONS)
 LOCAL_MODULE := ntfsfix
 LOCAL_MODULE_TAGS := eng
-LOCAL_SHARED_LIBRARIES := libext2_uuid
-LOCAL_SYSTEM_SHARED_LIBRARIES := libc
 LOCAL_STATIC_LIBRARIES := libfuse-lite libntfs-3g-lib
 include $(BUILD_EXECUTABLE)
 
@@ -65,9 +61,8 @@ LOCAL_SRC_FILES := ntfsprogs/attrdef.c ntfsprogs/boot.c ntfsprogs/sd.c ntfsprogs
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/include/fuse-lite $(LOCAL_PATH)/include/ntfs-3g \
 			$(LOCAL_PATH)/androidglue/include $(LOCAL_PATH)/ntfsprogs/ \
 			external/e2fsprogs/lib
-LOCAL_CFLAGS := -O2 -g -W -Wall -D_LARGEFILE_SOURCE -D_FILE_OFFSET_BITS=64 -DHAVE_CONFIG_H
+LOCAL_CFLAGS := $(CC_OPTIONS)
 LOCAL_MODULE := mkntfs
 LOCAL_MODULE_TAGS := eng
-LOCAL_SYSTEM_SHARED_LIBRARIES := libc
 LOCAL_STATIC_LIBRARIES := libfuse-lite libntfs-3g-lib
 include $(BUILD_EXECUTABLE)
