8c7c57279f9df73d3e2538c8a050cd7cb791ae2f
Lurker00 <Lurker00@users.noreply.github.com>
2015-06-15 15:42:41 +0300

    Возможность собирать под mips и clang (отключены)

--- jni/Application.mk
+++ jni/Application.mk
@@ -1,2 +1,13 @@
 APP_ABI := x86 x86_64 armeabi armeabi-v7a arm64-v8a
+#APP_ABI := all
 APP_PLATFORM := android-17
+
+APP_CFLAGS += -fno-stack-protector
+
+#NDK_TOOLCHAIN_VERSION=clang
+
+ifeq ($(NDK_TOOLCHAIN_VERSION),clang)
+	APP_CFLAGS += -mno-unaligned-access
+	APP_CFLAGS += -Wno-typedef-redefinition
+else
+endif
